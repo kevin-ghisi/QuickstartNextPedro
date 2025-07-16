830cae44cb35dffb606246c81a877d9ee5b42492
CarvoeirosRoboTech <carvoeirosrobotech@gmail.com>
2025-07-16 17:43:51 -0300

    Teste da Husky Lens

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/AutoBasket.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/AutoBasket.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.opModes;
 
 
 import static com.rowanmcalpin.nextftc.ftc.OpModeData.hardwareMap;
+import org.firstinspires.ftc.teamcode.opModes.SensorHuskyLens;
 
 import com.pedropathing.follower.Follower;
 import com.pedropathing.localization.Pose;
@@ -10,6 +11,7 @@ import com.pedropathing.pathgen.BezierLine;
 import com.pedropathing.pathgen.Path;
 import com.pedropathing.pathgen.PathChain;
 import com.pedropathing.pathgen.Point;
+import com.qualcomm.hardware.dfrobot.HuskyLens;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.rowanmcalpin.nextftc.core.command.Command;
@@ -23,14 +25,33 @@ import com.rowanmcalpin.nextftc.pedro.FollowPath;
 import org.firstinspires.ftc.teamcode.pedroPathing.constants.FConstants;
 import org.firstinspires.ftc.teamcode.pedroPathing.constants.LConstants;
 
-@Autonomous(name = "Auto teste 2")
-public class AutoTest2 extends PedroOpMode {
+@Autonomous(name = "Auto Basket")
+public class AutoBasket extends PedroOpMode {
 
-    private final Pose startPose = new Pose(8.09726443768997, 88.6322188449848, Math.toRadians(0.0));
+//    private final Pose startPose = new Pose(8.09726443768997, 88.6322188449848, Math.toRadians(0.0));
+    private final Pose startPose = new Pose(6.800, 88.632, Math.toRadians(0.0));
 
-    private Path basket, clipe;
+    private Path basket, estacionar, clipe;
+
 
     public void buildPaths()
+    {
+        basket = new Path(
+                new BezierCurve(
+                        new Point(6.800, 88.632, Point.CARTESIAN),
+                        new Point(33.264, 110.079, Point.CARTESIAN),
+                        new Point(12.474, 130.213, Point.CARTESIAN)
+                ));
+        basket.setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(135));
+
+        estacionar = new Path(
+                new BezierCurve(
+                        new Point(12.474, 130.213, Point.CARTESIAN),
+                        new Point(54.274, 118.176, Point.CARTESIAN),
+                        new Point(73.313, 93.666, Point.CARTESIAN)
+                ));
+            estacionar.setTangentHeadingInterpolation();
+    }
     {
         clipe = new Path(
                 new BezierLine(
@@ -50,12 +71,13 @@ public class AutoTest2 extends PedroOpMode {
     public Command secondRoutine() {
         return new SequentialGroup(
                 new ParallelGroup(
-                        new FollowPath(clipe)
-//                        Lift.INSTANCE.toHigh()
-                ),
-                new ParallelGroup(
                         new FollowPath(basket)
+//                        new FollowPath(clipe)
+////                        Lift.INSTANCE.toHigh()
                 )
+//                new ParallelGroup(
+//                        new FollowPath(basket)
+//                )
 //                new ParallelGroup(
 //                        Claw.INSTANCE.open(),
 //                        Lift.INSTANCE.toMiddle()
