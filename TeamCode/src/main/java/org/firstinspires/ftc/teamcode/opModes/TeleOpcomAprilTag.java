649961a65b0acb50a6b84fc94588af777889a3b1
CarvoeirosRoboTech <carvoeirosrobotech@gmail.com>
2025-07-23 17:25:17 -0300

    Teste do elevador

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/TeleOpcomAprilTag.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/TeleOpcomAprilTag.java
@@ -10,29 +10,41 @@ import com.qualcomm.hardware.dfrobot.HuskyLens;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.rowanmcalpin.nextftc.core.command.Command;
+import com.rowanmcalpin.nextftc.core.command.groups.ParallelGroup;
+import com.rowanmcalpin.nextftc.core.command.groups.SequentialGroup;
+import com.rowanmcalpin.nextftc.core.control.controllers.PIDFController;
 import com.rowanmcalpin.nextftc.ftc.driving.MecanumDriverControlled;
 import com.rowanmcalpin.nextftc.ftc.hardware.controllables.MotorEx;
+import com.rowanmcalpin.nextftc.ftc.hardware.controllables.RunToPosition;
+import com.rowanmcalpin.nextftc.ftc.hardware.controllables.RunToVelocity;
+import com.rowanmcalpin.nextftc.ftc.hardware.controllables.SetPower;
 import com.rowanmcalpin.nextftc.pedro.FollowPath;
 import com.rowanmcalpin.nextftc.pedro.PedroOpMode;
 import com.pedropathing.pathgen.Path;
+
 
 import org.firstinspires.ftc.teamcode.pedroPathing.constants.FConstants;
 import org.firstinspires.ftc.teamcode.pedroPathing.constants.LConstants;
 
 @TeleOp(group = "Basket Azul com AT")
 public class TeleOpcomAprilTag extends PedroOpMode {
-    public String frontLeftName = "frontLeft";
-    public String frontRightName = "frontRight";
-    public String backLeftName = "backLeft";
-    public String backRightName = "backRight";
+    public String frontLeftName = "leftFront";
+    public String frontRightName = "rightFront";
+    public String backLeftName = "leftRear";
+    public String backRightName = "rightRear";
+    public String elevadorEsquerdoName = "elevadorEsquerdo";
+    public String elevadorDireitoName = "elevadorDireito";
     public MotorEx frontLeftMotor;
     public MotorEx frontRightMotor;
     public MotorEx backLeftMotor;
     public MotorEx backRightMotor;
+    public MotorEx elevadorEsquerdoMotor;
+    public MotorEx elevadorDireitoMotor;
 
     public MotorEx[] motors;
+    public MotorEx[] elevadores;
     public Command driverControlled;
-    private HuskyLens huskyLens;
+//    private HuskyLens huskyLens;
     private boolean tagHandle = false;
     private final Pose startPose = new Pose(73.313, 93.666, Math.toRadians(0.135));
     private Follower follower;
@@ -47,17 +59,22 @@ public class TeleOpcomAprilTag extends PedroOpMode {
         backLeftMotor = new MotorEx(backLeftName);
         backRightMotor = new MotorEx(backRightName);
         frontRightMotor = new MotorEx(frontRightName);
+        elevadorEsquerdoMotor = new MotorEx(elevadorEsquerdoName);
+        elevadorDireitoMotor = new MotorEx(elevadorDireitoName);
 
         frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         frontRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         backRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        elevadorEsquerdoMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        elevadorDireitoMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
         motors = new MotorEx[]{frontLeftMotor, frontRightMotor, backLeftMotor, backRightMotor};
+        elevadores = new MotorEx[]{elevadorDireitoMotor, elevadorEsquerdoMotor};
 
         //Husky Lens
-        huskyLens = hardwareMap.get(HuskyLens.class, "Husky Lens");
-        huskyLens.selectAlgorithm(HuskyLens.Algorithm.TAG_RECOGNITION);
+//        huskyLens = hardwareMap.get(HuskyLens.class, "Husky Lens");
+//        huskyLens.selectAlgorithm(HuskyLens.Algorithm.TAG_RECOGNITION);
 
         follower = new Follower(hardwareMap, FConstants.class, LConstants.class);
         follower.setStartingPose(startPose);
@@ -69,6 +86,23 @@ public class TeleOpcomAprilTag extends PedroOpMode {
         driverControlled.invoke();
 
         follower.startTeleopDrive();
+
+        gamepadManager.getGamepad1().getRightBumper().setPressedCommand(() ->
+                 new ParallelGroup(
+                        new RunToPosition(elevadorEsquerdoMotor, elevadorEsquerdoMotor.getCurrentPosition() + 100,
+                                new PIDFController(1.0,0,0)),
+                        new RunToPosition(elevadorDireitoMotor, elevadorDireitoMotor.getCurrentPosition() + 100,
+                                new PIDFController(1.0,0,0))
+                )
+        );
+
+        gamepadManager.getGamepad1().getLeftBumper().setPressedCommand(() ->
+                new ParallelGroup(
+                        new RunToPosition(elevadorEsquerdoMotor, elevadorEsquerdoMotor.getCurrentPosition() - 100,
+                                new PIDFController(1.0,0,0)),
+                        new RunToPosition(elevadorDireitoMotor, elevadorDireitoMotor.getCurrentPosition() - 100,
+                                new PIDFController(1.0,0,0))
+                ));
     }
 
 
@@ -78,26 +112,26 @@ public class TeleOpcomAprilTag extends PedroOpMode {
         telemetry.addData("Y", follower.getPose().getY());
         telemetry.update();
 
-        HuskyLens.Block[] tags = huskyLens.blocks();
-
-        if (tags.length > 0) {
-            int tagId = tags[0].id;
-            telemetry.addData("Tag Detected", tagId);
-
-            if (gamepad1.cross) {
-                switch (tagId) {
-                    case 1:
-                        BezierCurve curve = new BezierCurve(
-                                follower.getPose(), new Pose(24.948, 127.587, Point.CARTESIAN),
-                                new Pose(20.790, 135.684, Point.CARTESIAN),
-                                new Pose(13.787, 129.994, Point.CARTESIAN)
-                        );
-                        AtBasketAzul.setLinearHeadingInterpolation(Math.toRadians(90), Math.toRadians(135));
-                        break;
-                }
-            }
-        } else {
-            telemetry.addLine("Sem TAG");
-        }
+//        HuskyLens.Block[] tags = huskyLens.blocks();
+//
+//        if (tags.length > 0) {
+//            int tagId = tags[0].id;
+//            telemetry.addData("Tag Detected", tagId);
+//
+//            if (gamepad1.cross) {
+//                switch (tagId) {
+//                    case 1:
+//                        BezierCurve curve = new BezierCurve(
+//                                follower.getPose(), new Pose(24.948, 127.587, Point.CARTESIAN),
+//                                new Pose(20.790, 135.684, Point.CARTESIAN),
+//                                new Pose(13.787, 129.994, Point.CARTESIAN)
+//                        );
+//                        AtBasketAzul.setLinearHeadingInterpolation(Math.toRadians(90), Math.toRadians(135));
+//                        break;
+//                }
+//            }
+//        } else {
+//            telemetry.addLine("Sem TAG");
+//        }
     }
 }
